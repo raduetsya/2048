835dcebc6945dc872cbb699a1e4c30cbb4dba1e2
lanus1401 <lanus.1401@gmail.com>
2014-04-03 23:16:32 +0700

    Implemented GestureRecognizer

--- game2048/src/com/lanus/game2048/view/GestureRecognizer.java
+++ game2048/src/com/lanus/game2048/view/GestureRecognizer.java
@@ -8,31 +8,43 @@ import com.lanus.game2048.GameGrid.DIRECTION;
  * Note: methods onPress(), onUnpress(), onMove() return true, if user 
  * made an action with his gesture. Usually onPress() and onMove()
  * returns true, but who knows how are you going to upgrade this class.
+ * getLastDir() returns direction from last user action, if you mind.
+ * 
  */
 public class GestureRecognizer {
 	
+	private static final int MINIMAL_OFFSET = 15;
+	
 	private Context context;
 	private DIRECTION direction;
+	
+	private DIRECTION lastDir;
+	
+	int gestCenterX, gestCenterY;
 	int gestOffsetX, gestOffsetY;
 	
 	public GestureRecognizer(Context context) {
 		this.context = context;
-		
-		// TODO Implement method
 	}
 
 	public boolean onPress(int x, int y) {
-		// TODO Implement method
-		return false;
+		gestCenterX = x; gestCenterY = y;
+		gestOffsetX = 0; gestOffsetY = 0;
+		direction = DIRECTION.NONE;
+		return true;
 	}
 	
 	public boolean onUnpress(int x, int y) {
-		// TODO Implement method
+		if (getOffset() >= MINIMAL_OFFSET) {
+			lastDir = getDirection();
+			return true;
+		}
 		return false;
 	}
 	
 	public boolean onMove(int x, int y) {
-		// TODO Implement method
+		gestOffsetX = x - gestCenterX;
+		gestOffsetY = y - gestCenterY;
 		return false;		
 	}
 	
@@ -43,9 +55,13 @@ public class GestureRecognizer {
 		return (direction != DIRECTION.NONE);
 	}
 	
+	public DIRECTION getLastDir() {
+		return lastDir;
+	}
+
 	/**
 	 * Returns offset in px for current gesture.
-	 * If you want direction, use getSignX() and getSignY().
+	 * If you want direction, use getSignX()/getSignY().
 	 */
 	public int getOffset() {
 		return Math.max( Math.abs(gestOffsetX), Math.abs(gestOffsetY) );
@@ -73,5 +89,13 @@ public class GestureRecognizer {
 		} else {
 			return 0;
 		}		
+	}
+	
+	private DIRECTION getDirection() {
+		if (getSignX() == 1) return DIRECTION.RIGHT;
+		if (getSignX() == -1) return DIRECTION.LEFT;
+		if (getSignY() == 1) return DIRECTION.DOWN;
+		if (getSignY() == -1) return DIRECTION.UP;
+		return DIRECTION.NONE;
 	}
 }
